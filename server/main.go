5fd7f3062643ab7e12cecf5c22966a4aaa59f995
Evan Carr <ejrcarr@gmail.com>
2023-03-05 14:23:30 -0500

    Healthcheck

--- server/main.go
+++ server/main.go
@@ -1,5 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+}
+
 func main() {
-	fmt.Print("Hello world")
+	fmt.Println("Hello world")
+
+	app := fiber.New()
+	app.Get("/healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	log.Fatal(app.Listen(":4000"))
 }
